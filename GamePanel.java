d56dac024b024f0837bb324f683cc333e4b028e8
Boryana Baeva <borenceto@gmail.com>
2016-04-19 16:25:54 +0300

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -2,9 +2,12 @@ package org.softuni;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.util.*;
 
-public class GamePanel extends JPanel implements Runnable{
+public class GamePanel extends JPanel implements Runnable, KeyListener {
 
     // fields
     public static int WIDTH = 400;
@@ -14,33 +17,61 @@ public class GamePanel extends JPanel implements Runnable{
     private boolean running;
 
     private BufferedImage image;   // canvas
-    private  Graphics2D g;         // paintbrush
+    private Graphics2D g;         // paintbrush
 
     private int FPS = 30;          // Setting frames per second
     private double averageFPS;
 
+    public static Player player;
+    public static ArrayList<Bullet> bullets;
+    public static ArrayList<Enemy> enemies;
+
+    private long waveStartTimer;
+    private long waveStartTimerDiff;
+    private int waveNumber;
+    private boolean waveStart;
+    private int waveDelay;
+
     // constructor
-    public GamePanel(){
+    public GamePanel() {
         super();
-        setPreferredSize(new Dimension(WIDTH,HEIGHT));
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setFocusable(true);
         requestFocus();
     }
 
     // functions
-    public void addNotify(){
+    public void addNotify() {
         super.addNotify();
-        if (thread == null){
+        if (thread == null) {
             thread = new Thread(this);
             thread.start();
         }
+        addKeyListener(this);
     }
 
-    public void run(){
+    public void run() {
         running = true;
 
-        image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+        image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
         g = (Graphics2D) image.getGraphics();
+        g.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g.setRenderingHint(
+                RenderingHints.KEY_TEXT_ANTIALIASING,
+                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        player = new Player();
+        bullets = new ArrayList<Bullet>();
+        enemies = new ArrayList<Enemy>();
+
+        waveStartTimer = 0;
+        waveStartTimerDiff = 0;
+        waveStart = true;
+        waveNumber = 0;
+        waveDelay = 2000;
 
         long startTime;
         long URDTimeMillis;
@@ -52,7 +83,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         long targetTime = 1000 / FPS;  // the amount of time that it takes for 1 loop to run in order to maintain 30fps
         // game loop
-        while (running){
+        while (running) {
 
             startTime = System.nanoTime();      // Gets the current time in nanoseconds
 
@@ -63,7 +94,7 @@ public class GamePanel extends JPanel implements Runnable{
             URDTimeMillis = (System.nanoTime() - startTime) / 1000000;   // dividing to convert the result from nanoseconds to milliseconds
             waitTime = targetTime - URDTimeMillis;                       // the amount of extra time we need to wait
 
-            try{
+            try {
                 Thread.sleep(waitTime);
             } catch (Exception e) {
             }
@@ -71,103 +102,198 @@ public class GamePanel extends JPanel implements Runnable{
             totalTime += System.nanoTime() - startTime;
             frameCount++;
 
-            if (frameCount == maxFrameCount){                            // to put a speed limit on the game loop
-                averageFPS = 1000.0 / ((totalTime/frameCount)/1000000);
+            if (frameCount == maxFrameCount) {                            // to put a speed limit on the game loop
+                averageFPS = 1000.0 / ((totalTime / frameCount) / 1000000);
                 frameCount = 0;
-                totalTime =0;
+                totalTime = 0;
             }
         }
     }
 
-    private void gameUpdate(){    // here goes all of the game logic
-		
-		//new wave
-		if (waveStartTimer == 0 && enemies.size() == 0) {
-			waveNumber++;
-			waveStart = false;
-			waveStartTimer = System.nanoTime()
-		}
-		else {
-			waveStartTimerDiff = (System.nanoTime() - waveStartTimer) / 1000000;
-			if (waveStartTimerDiff > waveDelay) {
-				waveStart = true;
-				waveStartTimer = 0;
-				waveStartTimerDiff = 0;
-			}
-		}
-		
-		// create enemies
-		if (waveStart && enemies.size() == 0) {
-			createNewEnemies();
-		}
-		
-		// player update
-		player.update();
-		
-		// bullet update
-		for(int i = 0; i < bullets.size(); i++) {
-			boolean remove = bullets.get(i).update();
-			if (remove) {
-				bullets.remove(i);
-				i--;
-			}
-		}
-		
-		// enemy update
-		for (int i = 0; i < enemies.size(); i++) {
-			enemies.get(i).update;
-		}
-		
-		// bullet-enemy collision
-		for (int i = 0; < bull.size(); i++) {
-			
-			Bullet b = bullets.get(i);
-			double bx = b.getx();
-			double by = b.gety();
-			double br = b.getr();
-			
-			for (int j = 0; j < enemies.size(); j++) {
-				
-				Enemy e = enemies.get(k);
-				double ex = e.getx();
-				double ey = e.gety();
-				double er = e.getr();
-				
-				double dx = bx - ex;
-				double dy = by - ey;
-				double dist = Math.sqrt(dx * dx + dy * dy);
-				
-				if (dist < br + er) {
-					e.hit();
-					bullets.remove(i);
-					i--;
-					break;
-				}
-			}
-			
-			// player-enemy collision
-		}
-		
-		// check dead enemies
-		for (int i = 0; enemies.size(); i++) {
-			if (enemies.get(i).isDead()) {
-				enemies.remove(i);
-				i--;
-			}
-		}
+    private void gameUpdate() {    // here goes all of the game logic
+
+        // new wave
+        if (waveStartTimer == 0 && enemies.size() == 0) {
+            waveNumber++;
+            waveStart = false;
+            waveStartTimer = System.nanoTime();
+        } else {
+            waveStartTimerDiff = (System.nanoTime() - waveStartTimer) / 1000000;
+            if (waveStartTimerDiff > waveDelay) {
+                waveStart = true;
+                waveStartTimer = 0;
+                waveStartTimerDiff = 0;
+            }
+        }
+
+        // create enemies
+        if (waveStart && enemies.size() == 0) {
+            createNewEnemies();
+        }
+
+        //player update
+        player.update();
+
+        // bullet update
+        for (int i = 0; i < bullets.size(); i++) {
+            boolean remove = bullets.get(i).update();
+            if (remove) {
+                bullets.remove(i);
+                i--;
+            }
+        }
+
+        // enemy update
+        for (int i = 0; i < enemies.size(); i++) {
+            enemies.get(i).update();
+        }
+
+        // bullet-enemy collision
+        for (int i = 0; i < bullets.size(); i++) {
+
+            Bullet b = bullets.get(i);
+            double bx = b.getx();
+            double by = b.gety();
+            double br = b.getr();
+
+            for (int j = 0; j < enemies.size(); j++) {
+
+                Enemy e = enemies.get(j);
+                double ex = e.getx();
+                double ey = e.gety();
+                double er = e.getr();
+
+                double dx = bx - ex;
+                double dy = by - ey;
+                double dist = Math.sqrt(dx * dx + dy * dy);
+
+                if (dist < br + er) {
+                    e.hit();                  // enemy gets hit
+                    bullets.remove(i);        // the bullet is removed
+                    i--;
+                    break;
+                }
+            }
+        }
+        // check dead enemies
+        for (int i = 0; i < enemies.size(); i++) {
+            if (enemies.get(i).isDead()) {
+                enemies.remove(i);
+                i--;
+            }
+        }
     }
 
-    private  void gameRender(){   // offscreen image (double buffering)
 
-        g.setColor(Color.gray);
-        g.fillRect(0,0,WIDTH,HEIGHT);
-        g.setColor(Color.black);
-        g.drawString("FPS: " + averageFPS,10,10);
+    private void gameRender() {   // offscreen image (double buffering)
+
+        // draw background
+        g.setColor(new Color(0, 100, 255));
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        // draw player
+        player.draw(g);
+
+        // draw bullets
+        for (int i = 0; i < bullets.size(); i++) {
+
+            bullets.get(i).draw(g);
+        }
+
+        // draw enemy
+        for (int i = 0; i < enemies.size(); i++) {
+            enemies.get(i).draw(g);
+        }
+
+        // draw wave number
+        if (waveStartTimer != 0) {
+            g.setFont(new Font("Century Gothic", Font.PLAIN, 18));
+            String s = "- W A V E   " + waveNumber + "   -";
+            int length = (int) g.getFontMetrics().getStringBounds(s, g).getWidth();
+            int alpha = (int) (255 * Math.sin(3.14 * waveStartTimerDiff / waveDelay));
+            if (alpha > 255) {
+                alpha = 255;
+            }
+                g.setColor(new Color(255, 255, 255, alpha));
+                g.drawString(s, WIDTH / 2 - length / 2, HEIGHT / 2);
+
+        }
+
+        // draw player lives
+        for (int i = 0; i < player.getLives(); i++) {
+            g.setColor(Color.WHITE);
+            g.fillOval(20 + (20 * i),20, player.getr() * 2,player.getr()*2);
+            g.setStroke(new BasicStroke(3));
+            g.setColor(Color.WHITE.darker());
+            g.drawOval(20 + (20 * i),20, player.getr() * 2,player.getr()*2);
+            g.setStroke(new BasicStroke(1));
+        }
+
     }
-
-    private void gameDraw(){      // drawing on the screen
+    private void gameDraw() {      // drawing on the screen
         Graphics g2 = this.getGraphics();
-        g2.drawImage(image,0,0, null);
+        g2.drawImage(image, 0, 0, null);
         g2.dispose();
+    }
+
+    private void createNewEnemies() {
+
+        enemies.clear();
+        Enemy e;
+
+        if (waveNumber == 1) {
+            for (int i = 0; i < 4; i++) {
+                enemies.add(new Enemy(1, 1));
+            }
+        }
+        if (waveNumber == 2) {
+            for (int i = 0; i < 8; i++) {
+                enemies.add(new Enemy(1, 1));
+            }
+        }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent key) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent key) {
+        int keyCode = key.getKeyCode();
+        if (keyCode == KeyEvent.VK_LEFT) {
+            player.setLeft(true);
+        }
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            player.setRight(true);
+        }
+        if (keyCode == KeyEvent.VK_UP) {
+            player.setUp(true);
+        }
+        if (keyCode == KeyEvent.VK_DOWN) {
+            player.setDown(true);
+        }
+        if (keyCode == KeyEvent.VK_Z) {
+            player.setFiring(true);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent key) {
+        int keyCode = key.getKeyCode();
+        if (keyCode == KeyEvent.VK_LEFT) {
+            player.setLeft(false);
+        }
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            player.setRight(false);
+        }
+        if (keyCode == KeyEvent.VK_UP) {
+            player.setUp(false);
+        }
+        if (keyCode == KeyEvent.VK_DOWN) {
+            player.setDown(false);
+        }
+        if (keyCode == KeyEvent.VK_Z) {
+            player.setFiring(false);
+        }
     }
 }
