a3eac45d2498e8cffa2df15b0daa3d307c45fe4d
Mihail Chipilov <m.chipilov@gmail.com>
2016-04-19 12:12:07 +0300

    Added files via upload

---
+++ Enemy.java
@@ -0,0 +1,20 @@
+// FUNCTIONS
+public double getx() {
+	return x;
+}
+
+public double gety() {
+	return y;
+}
+
+public double getr() {
+	return r;
+}
+
+public int getType() {
+	return type;
+}
+
+public int getRank() {
+	return rank;
+}
